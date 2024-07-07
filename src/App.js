d5292e22f8da06f870de78131597ef4679892aaf
Iago Siqueira <iago.cith@hotmail.com>
2024-07-07 14:50:37 -0300

    add first pages login registrations and password recover

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Login from './pages/Login.tsx';
+import Signup from './pages/Signup.tsx';
+import RecoverPassword from './pages/RecoverPassword.tsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/recover-passoword" element={<RecoverPassword />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
